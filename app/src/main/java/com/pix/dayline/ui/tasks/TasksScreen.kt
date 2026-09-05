a773e269bf07a6367bdda1bfd9b11733acdf5036
Aness Chelfat <ns.chlfat@gmail.com>
2026-09-05 09:29:11 +0200

    Fix Compose weight imports

--- app/src/main/java/com/pix/dayline/ui/tasks/TasksScreen.kt
+++ app/src/main/java/com/pix/dayline/ui/tasks/TasksScreen.kt
@@ -16,7 +16,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBars
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
